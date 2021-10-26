2b4c6e8c7bf88d687e30d9ce5d087b3aa9b35f9b
natanhp <natanhp@protonmail.com>
2021-10-27 05:53:33 +0700

    Add data not found exception

---
+++ Movie-Detail-Service/src/main/java/id/natanhp/moviedetailservice/exception/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package id.natanhp.moviedetailservice.exception;
+
+public class DataNotFoundException extends Exception {
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
