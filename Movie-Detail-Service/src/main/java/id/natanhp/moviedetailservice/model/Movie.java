ec62404f5280f0e9aa36e865d93e5655ef345611
natanhp <natanhp@protonmail.com>
2021-10-27 05:40:29 +0700

    Add models

---
+++ Movie-Detail-Service/src/main/java/id/natanhp/moviedetailservice/model/Movie.java
@@ -0,0 +1,24 @@
+package id.natanhp.moviedetailservice.model;
+
+import lombok.Data;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import java.util.List;
+
+@Data
+@Node
+public class Movie {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    private String title;
+    private int released;
+    private String tagline;
+
+    @Relationship(type = "ACTED_IN", direction = Relationship.Direction.INCOMING)
+    private List<Role> roles;
+}
---
+++ Movie-Detail-Service/src/main/java/id/natanhp/moviedetailservice/model/Person.java
@@ -0,0 +1,16 @@
+package id.natanhp.moviedetailservice.model;
+
+import lombok.Data;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+@Data
+@Node
+public class Person {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    private String name;
+}
