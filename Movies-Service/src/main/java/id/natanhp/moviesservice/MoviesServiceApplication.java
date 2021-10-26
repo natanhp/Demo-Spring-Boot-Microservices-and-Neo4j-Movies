e686ea107a917a7f50a5ce773db118308d37df41
natanhp <natanhp@protonmail.com>
2021-10-26 09:24:33 +0700

    Enable eureka client

--- Movies-Service/src/main/java/id/natanhp/moviesservice/MoviesServiceApplication.java
+++ Movies-Service/src/main/java/id/natanhp/moviesservice/MoviesServiceApplication.java
@@ -2,8 +2,10 @@ package id.natanhp.moviesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MoviesServiceApplication {
 
     public static void main(String[] args) {
