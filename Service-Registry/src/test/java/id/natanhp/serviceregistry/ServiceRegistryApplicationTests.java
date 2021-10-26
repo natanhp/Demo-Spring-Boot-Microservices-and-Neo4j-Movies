0d5aec3265faf2122d9610bc2ff363874e7a2072
natanhp <natanhp@protonmail.com>
2021-10-26 08:37:34 +0700

    Service-Registry Initial commit

---
+++ Service-Registry/src/test/java/id/natanhp/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package id.natanhp.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
