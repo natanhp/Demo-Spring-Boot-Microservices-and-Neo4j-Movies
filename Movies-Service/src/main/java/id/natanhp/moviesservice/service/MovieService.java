18923561d29f43bd40841e34ad69ab03c835d246
natanhp <natanhp@protonmail.com>
2021-10-27 03:05:04 +0700

    Create service for movie

---
+++ Movies-Service/src/main/java/id/natanhp/moviesservice/service/MovieService.java
@@ -0,0 +1,25 @@
+package id.natanhp.moviesservice.service;
+
+import id.natanhp.moviesservice.model.Movie;
+import id.natanhp.moviesservice.repository.MovieRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public Movie findByTitle(String title) {
+        return movieRepository.findByTitle(title);
+    }
+
+    public Iterable<Movie> findAll() {
+        return movieRepository.findAll();
+    }
+}
