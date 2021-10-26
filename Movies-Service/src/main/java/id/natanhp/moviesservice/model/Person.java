ea05a480461ec7858d27a4c8be41a4fe22269799
natanhp <natanhp@protonmail.com>
2021-10-27 03:05:15 +0700

    Create model for person

---
+++ Movies-Service/src/main/java/id/natanhp/moviesservice/model/Person.java
@@ -0,0 +1,17 @@
+package id.natanhp.moviesservice.model;
+
+import lombok.Data;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+@Data
+@Node
+public class Person {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    private String name;
+//    private int born;
+}
