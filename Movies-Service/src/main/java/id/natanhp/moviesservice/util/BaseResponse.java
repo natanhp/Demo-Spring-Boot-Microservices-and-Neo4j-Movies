25dfc5d542a8a4dd10d16c9330b1f4c444c87bc5
natanhp <natanhp@protonmail.com>
2021-10-27 03:16:13 +0700

    Add base response

---
+++ Movies-Service/src/main/java/id/natanhp/moviesservice/util/BaseResponse.java
@@ -0,0 +1,18 @@
+package id.natanhp.moviesservice.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseResponse {
+    public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Object content) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", message);
+        response.put("status", status.value());
+        response.put("content", content);
+
+        return new ResponseEntity<>(response, status);
+    }
+}
