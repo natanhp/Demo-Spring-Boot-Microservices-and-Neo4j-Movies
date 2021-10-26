e251e6988ead7c3c88b99989e66ebb6c5d1a4ccc
natanhp <natanhp@protonmail.com>
2021-10-27 04:55:05 +0700

    Create custom exception For handling invalid user input

---
+++ Movies-Service/src/main/java/id/natanhp/moviesservice/exception/InvalidUserInputException.java
@@ -0,0 +1,7 @@
+package id.natanhp.moviesservice.exception;
+
+public class InvalidUserInputException extends Exception {
+    public InvalidUserInputException(String message) {
+        super(message);
+    }
+}
