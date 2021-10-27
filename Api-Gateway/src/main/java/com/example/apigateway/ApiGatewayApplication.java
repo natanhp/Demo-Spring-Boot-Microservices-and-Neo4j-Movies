bb391c4d1ba6ee7e5bdf4ea0e17b96aa716e4d18
natanhp <natanhp@protonmail.com>
2021-10-27 10:01:56 +0700

    Enable routing

--- Api-Gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ Api-Gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -2,15 +2,28 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class ApiGatewayApplication {
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/**")
+                        .uri("http://localhost:5000"))
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
+
+
 
