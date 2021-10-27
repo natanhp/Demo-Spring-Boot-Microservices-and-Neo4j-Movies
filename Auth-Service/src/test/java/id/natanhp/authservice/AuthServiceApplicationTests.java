0715499adcb007e3a2d99e0e38c221c0e87f4677
natanhp <natanhp@protonmail.com>
2021-10-28 01:53:53 +0700

    Auth service initial commit

---
+++ Auth-Service/src/test/java/id/natanhp/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.natanhp.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
