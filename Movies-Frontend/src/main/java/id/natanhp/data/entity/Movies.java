0be4e7227a1eda71bb8b1fb6b322651577bdd2b7
natanhp <natanhp@protonmail.com>
2021-10-29 14:49:54 +0700

    Add new constructor

--- Movies-Frontend/src/main/java/id/natanhp/data/entity/Movies.java
+++ Movies-Frontend/src/main/java/id/natanhp/data/entity/Movies.java
@@ -1,8 +1,10 @@
 package id.natanhp.data.entity;
 
+import javax.annotation.Nullable;
 import javax.persistence.Entity;
 
 import id.natanhp.data.AbstractEntity;
+import id.natanhp.data.dto.RequestBodyMovie;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.vaadin.fusion.Nonnull;
@@ -27,6 +29,12 @@ public class Movies extends AbstractEntity {
         this.released = released;
     }
 
+    public Movies(RequestBodyMovie requestBodyMovie) {
+        this.id = requestBodyMovie.getId();
+        this.title = requestBodyMovie.getTitle();
+        this.released = requestBodyMovie.getReleased();
+    }
+
     public String getTitle() {
         return title;
     }
@@ -38,6 +46,10 @@ public class Movies extends AbstractEntity {
     }
     public void setReleased(String released) {
         this.released = released;
+    }
+
+    public void setId(@Nullable Long id) {
+        this.id = id;
     }
 
 }
