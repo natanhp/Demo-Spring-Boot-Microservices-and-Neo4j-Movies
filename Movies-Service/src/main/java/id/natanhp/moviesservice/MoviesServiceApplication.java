b073d11ae6cf0abaf166369dca398328ecd2cd7a
natanhp <natanhp@protonmail.com>
2021-11-24 16:40:53 +0700

    Remove configuration

--- Movies-Service/src/main/java/id/natanhp/moviesservice/MoviesServiceApplication.java
+++ Movies-Service/src/main/java/id/natanhp/moviesservice/MoviesServiceApplication.java
@@ -2,11 +2,7 @@ package id.natanhp.moviesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,14 +11,5 @@ public class MoviesServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MoviesServiceApplication.class, args);
     }
-
 }
 
-@Configuration
-class RestTemplateConfig {
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-}
